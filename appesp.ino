a5b58dff3b61d1ad3526c7db815c02e8d062ee30
dhruvbhandari-0707 <dhruvbhan2005@gmail.com>
2025-11-22 10:24:10 +0530

    Update appesp.ino

--- appesp.ino
+++ appesp.ino
@@ -1,16 +1,4 @@
-// Copyright 2015-2016 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+
 #include "esp_http_server.h"
 #include "esp_timer.h"
 #include "esp_camera.h"
@@ -36,11 +24,11 @@
 #define FACE_COLOR_PURPLE (FACE_COLOR_BLUE | FACE_COLOR_RED)
 
 typedef struct {
-        size_t size; //number of values used for filtering
-        size_t index; //current value index
-        size_t count; //value count
+        size_t size; 
+        size_t index; 
+        size_t count; 
         int sum;
-        int * values; //array to be filled with values
+        int * values; 
 } ra_filter_t;
 
 typedef struct {
@@ -197,7 +185,7 @@ static int run_face_recognition(dl_matrix3du_t *image_matrix, box_array_t *net_b
         }
     } else {
         Serial.println("Face Not Aligned");
-        //rgb_print(image_matrix, FACE_COLOR_YELLOW, "Human Detected");
+        
     }
 
     dl_matrix3du_free(aligned_face);
@@ -648,3 +636,4 @@ void startCameraServer(){
         httpd_register_uri_handler(stream_httpd, &stream_uri);
     }
 }
+
